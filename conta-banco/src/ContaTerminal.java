3a4dafd17c6cbb060e5b5d8aaa77be1ecd7a4a32
talitamatos2 <talitamatosliver@gmail.com>
2024-07-03 20:48:30 -0300

    implementando codigo resolutivo

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        int numeroAgencia;
+        String nomeCliente;
+        double saldo;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da agência: ");
+        numeroAgencia = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.print("Por favor, digite seu nome completo: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.print("Por favor, digite o valor do saldo inicial: ");
+        saldo = scanner.nextDouble();
+
+        scanner.close();
+
+        String mensagemBoasVindas = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco! Sua agência é " + numeroAgencia + ", conta 0001 e seu saldo R$ " + saldo + " já está disponível para saque.";
+
+        System.out.println(mensagemBoasVindas);
     }
 }
